917b58d0512ea418a926b330742fa5ae282dea5b
Roger Correa <j.rogercorreamello@gmail.com>
2023-10-28 14:38:31 -0300

    add new feature
    adicionado função de listar despesas

--- script.js
+++ script.js
@@ -43,6 +43,25 @@ class Bd {
 
         localStorage.setItem('id', id)
     }
+
+    recuperarTodosRegistros() {
+
+        let despesas = []
+
+        let id = localStorage.getItem('id')
+
+        for(let i = 1; i <= id; i++) {
+            let despesa = JSON.parse(localStorage.getItem(i))
+
+            if(despesa === null) {
+                continue
+            }
+
+            despesas.push(despesa)
+        }
+
+        return despesas
+    }
 }
 
 let bd = new Bd()
@@ -74,6 +93,14 @@ function cadastrarDespesa() {
         document.getElementById('modalBtn').className = 'btn btn-success'
 
         $('#modalRegistroDespesa').modal('show')
+
+        ano.value = ''
+        mes.value = ''
+        dia.value = ''
+        tipo.value = ''
+        descricao.value = ''
+        valor.value = ''
+        
     } else {
 
         document.getElementById('modalTitulo').innerHTML = 'Erro na inclusão do registro'
@@ -84,4 +111,38 @@ function cadastrarDespesa() {
 
         $('#modalRegistroDespesa').modal('show')
     }
+}
+
+function carregaListaDespesas() {
+
+    let despesas = []
+
+    despesas = bd.recuperarTodosRegistros()
+
+    var listaDespesas = document.getElementById('listaDespesas')
+
+    despesas.forEach(function(d) {
+
+        console.log(d)
+
+        let linha = listaDespesas.insertRow()
+
+        linha.insertCell(0).innerHTML = `${d.dia}/${d.mes}/${d.ano}`
+
+        switch(parseInt(d.tipo)) {
+            case 1: d.tipo = 'Alimentação'
+                break
+            case 2: d.tipo = 'Educação'
+                break
+            case 3: d.tipo = 'Lazer'
+                break
+            case 4: d.tipo = 'Saúde'
+                break
+            case 5: d.tipo = 'Transporte'
+        }
+
+        linha.insertCell(1).innerHTML = d.tipo
+        linha.insertCell(2).innerHTML = d.descricao
+        linha.insertCell(3).innerHTML = d.valor
+    })
 }
